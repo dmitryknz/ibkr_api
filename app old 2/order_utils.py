10b45d205eebe14fc5fbc63f1d481f94ce77d8f8
Dmitry Knz <dknyazkov@gmail.com>
2025-06-09 17:03:14 +0200

    Добавлен логгер и обновлён ibkr_client

--- app old 2/order_utils.py
+++
@@ -1,7 +0,0 @@
-from ib_insync import *
-
-def create_unh_contract():
-    return Stock('UNH', 'SMART', 'USD')
-
-def create_limit_order(action="BUY", quantity=1, price=0.01):
-    return LimitOrder(action, quantity, price)
